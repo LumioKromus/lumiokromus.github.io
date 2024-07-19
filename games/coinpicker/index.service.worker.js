7948e1a74a673478cd8cdcde289aef9de1d9c85f
LumioKromus <lumiokromus@gmail.com>
2024-07-19 01:27:33 -0300

    a

--- games/coinpicker/index.service.worker.js
+++ games/coinpicker/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1721077485|53892264";
+const CACHE_VERSION = "1721345384|55691947";
 const CACHE_PREFIX = "coinpicker-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
--- games/makinaria/index.service.worker.js
+++ games/makinaria/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1721191916|68948202";
+const CACHE_VERSION = "1721344843|62915202";
 const CACHE_PREFIX = "Makinaria-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
